f2e6e4d19967881b020d16c025d95a899bbddc0b
Matt Wright <matt@nobien.net>
2016-03-07 13:10:58 -0500

    Add some logging statements to the application object

--- rittenhouse/app.py
+++ rittenhouse/app.py
@@ -63,34 +63,53 @@ class Rittenhouse(Application):
         aiohttp_jinja2.setup(self, loader=loader)
 
     async def _close_all_websockets(self, app):
+        async def _close_webcosket(ws):
+            await ws.close(message='Server shutdown')
+            logger.debug('websocket %s successfully closed', ws)
+
+        logger.info('closing %s open websockets', len(self._websockets))
+
         await asyncio.gather(*[
-            ws.close(message='Server shutdown')
+            _close_webcosket(ws)
             for ws in self._websockets
         ], loop=self.loop)
+
+        logger.info('websocket shutdown completed')
 
     async def websocket(self, request):
         websocket = WebSocketResponse()
         await websocket.prepare(request)
         self._websockets.append(websocket)
+        logger.info('registered websocket %s', websocket)
+
         async for msg in websocket:
             if msg.tp != MsgType.text:
+                logger.debug('received nontext message from %s', websocket)
                 break
+
         self._websockets.remove(websocket)
         await websocket.close()
+        logger.info('closed websocket %s', websocket)
+
         return websocket
 
     def broadcast(self, event, payload):
+        message = json.dumps({
+            'event': event,
+            'payload': payload
+        })
+
         async def _send_str(ws):
-            message = json.dumps({
-                'event': event,
-                'payload': payload
-            })
             ws.send_str(message)
+            logger.debug('message sent to %s', ws)
 
         coroutine = asyncio.gather(*[
             _send_str(ws)
             for ws in self._websockets
         ], loop=self.loop)
+
+        logger.info('broadcasting %s to %s websockets',
+                     message, len(self._websockets))
 
         asyncio.ensure_future(coroutine, loop=self.loop)
 
